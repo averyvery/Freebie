3fcb0d79187e0c8ab521a3431dec5a64f944ffc5
Doug Avery <dougunderscorenelson@gmail.com>
2010-07-10 00:09:01 -0400

    First commit. Fingers crossed!

---
+++ language/english/lang.freebie.php
@@ -0,0 +1,23 @@
+<?php
+
+$lang = array(
+  'to_disappear' => '<h4>Segments you want ExpressionEngine to ignore.</h4>
+                      <div style="margin: 20px 0 0 10px">
+                        <p>Accepted formats:</p>
+                        <ul>
+                          <li style="margin: 10px 0 10px 10px">
+                            <code>success|error|preview</code>
+                          </li>
+                          <li style="margin: 10px 0 10px 10px">
+                            <code>success error preview</code>
+                          </li>
+                          <li style="margin: 10px 0 10px 10px">
+                            <code>success<br />error<br />preview</code>
+                          </li>
+                          <li style="margin: 10px 0 10px 10px">
+                            Wildcards: <code>inky*clyde</code> matched <code>inkyblinkypinkyclyde</code>
+                          </li>                        
+                        </ul>
+                      </div>',
+  'remove_numbers' => 'Ignore numeric segments (ex: /2010/, /2/ /101/)'
+);
