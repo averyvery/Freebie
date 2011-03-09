fd60fe36e224137972138a7382f4dc7cbb466d57
Doug Avery <dougunderscorenelson@gmail.com>
2011-03-09 09:08:15 -0800

    Pointing to the new XML feed

--- freebie/config.php
+++ freebie/config.php
@@ -2,6 +2,6 @@
 
   $config['name']='Freebie';
   $config['version']='0.1.1';
-  $config['nsm_addon_updater']['versions_xml'] = 'http://github.com/averyvery/Freebie/raw/master/rss/nsm_addon_updater.rss';
+  $config['nsm_addon_updater']['versions_xml'] = 'http://github.com/averyvery/Freebie/raw/master/rss/nsm_addon_updater.xml';
   
 ?>
